6749057ab596a03df466bd24f8680cdbde573ebe
BevM <82781273+BevM-BD@users.noreply.github.com>
2021-11-24 15:47:37 -0500

    Bruh imagine this gets to be a popular mod, i would be so proud
    sdqsd

---
+++ example_mods/stages/artroom.lua
@@ -0,0 +1,17 @@
+function onCreate()
+
+makeLuaSprite('theBg','sussybg',-199.25,177.7)
+addLuaSprite('theBg',false)
+end
+
+function onBeatHit(...)--for every beat
+	-- body
+end
+
+function onStepHit(...)--for every step
+	-- body
+end
+
+function onUpdate(...)
+	-- body
+end
